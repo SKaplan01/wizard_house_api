af51c2ea42e2f3fbb925c16127a16ae0d675d6a0
Sarah Kaplan <skaplan520@gmail.com>
2019-01-29 12:11:48 -0800

    added routes to update and delete wizards

--- routes/wizards.js
+++ routes/wizards.js
@@ -3,9 +3,9 @@ const db = require('../db');
 
 const router = new express.Router();
 
+// select all wizards, returns id, name, house, image_url
 router.get('/', async function(req, res, next) {
   try {
-    // select all wizards, returns id, name, house, image_url
     const results = await db.query(`SELECT * FROM wizards`);
     return res.json({ wizards: results.rows });
   } catch (err) {
@@ -13,9 +13,9 @@ router.get('/', async function(req, res, next) {
   }
 });
 
+//add a new wizard, returns id, name, house, img_url
 router.post('/', async function(req, res, next) {
   try {
-    //add a new wizard, returns id, name, house, img_url
     let { name, house, img_url } = req.body;
     const result = await db.query(
       `INSERT INTO wizards (name, house, image_url)
@@ -24,6 +24,34 @@ router.post('/', async function(req, res, next) {
       [name, house, img_url]
     );
     return res.json({ wizard: result.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//update a wizard, returning the updated wizard
+router.patch('/:id', async function(req, res, next) {
+  try {
+    const { name, house, img_url } = req.body;
+    const result = await db.query(
+      `UPDATE wizards SET name=$1, house=$2, image_url=$3
+      WHERE id = $4
+      RETURNING id, name, house, image_url`,
+      [name, house, img_url, req.params.id]
+    );
+    return res.json(result.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//delete a wizard, returning {message: 'Deleted'}
+router.delete('/:id', async function(req, res, next) {
+  try {
+    const result = await db.query(`DELETE FROM wizards WHERE id= $1`, [
+      req.params.id
+    ]);
+    return res.json({ message: 'Deleted' });
   } catch (err) {
     return next(err);
   }
